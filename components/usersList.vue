54bad3bb305ffc6958240fbc1da542c993b51b7d
Salwe Breid <salwebreid@gmail.com>
2019-01-06 21:32:04 +0400

    Added mini User info when list element has selected

--- components/usersList.vue
+++ components/usersList.vue
@@ -1,10 +1,19 @@
 <template>
   <ul>
-    <li v-for="user in this.$store.state.users">
+    <li v-for="user in this.$store.state.users" @click="selectUser(user.id)" v-bind:class="{ selected: selectedUserId === user.id}">
       {{ user.name }} {{ user.surname }}
     </li>
   </ul>
 </template>
+
+<script>
+  export default {
+    props: {
+      selectUser: Function,
+      selectedUserId: Number,
+    }
+  }
+</script>
 
 <style lang="scss">
   ul {
@@ -15,6 +24,7 @@
       border: solid 1px #ccc;
       padding: 5px 10px;
       margin-top: -1px;
+      &.selected,
       &:hover {
          cursor: pointer;
          background: #f5f5f5;
